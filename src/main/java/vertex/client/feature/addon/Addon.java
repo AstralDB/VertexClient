11556ab979c3dda309100518aa183a6c701566f1
Astral Database <109240425+AstralDB@users.noreply.github.com>
2022-07-23 06:44:53 -0700

    sex?

--- src/main/java/vertex/client/feature/addon/Addon.java
+++ src/main/java/vertex/client/feature/addon/Addon.java
@@ -1,6 +1,4 @@
-/*
- * Copyright (c) 2022 Coffee Client, 0x150 and contributors. All rights reserved.
- */
+
 
 package vertex.client.feature.addon;
 
--- src/main/java/vertex/client/helper/network/package-info.java
+++ src/main/java/vertex/client/helper/network/package-info.java
@@ -1,6 +1,4 @@
-/*
- * Copyright (c) 2022 Coffee Client, 0x150 and contributors. All rights reserved.
- */
+
 
 /*
  * Everything here belongs to netty, I copied the files from their repo
